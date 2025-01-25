4befba119a7f660ad1a54f520e6b5b5267a46051
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-01-25 13:19:14 -0300

    Criando atributos e método na classe Ninja.

--- Ninja.java
+++ Ninja.java
@@ -1,5 +1,16 @@
 public class Ninja {
+    String nome;
+    int idade;
+    String missao;
+    String nivelDeDificuldade;
+    String statusDaMissao;
 
-
-
+    public void mostrarInformacoes() {
+        System.out.println("Informações do Ninja");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Missão: " + missao);
+        System.out.println("Nível de Dificuldade: " + nivelDeDificuldade);
+        System.out.println("Status da Missão: " + statusDaMissao);
+    }
 }
